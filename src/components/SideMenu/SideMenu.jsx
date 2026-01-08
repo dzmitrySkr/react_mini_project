f4a57c31286b73faf1817a964edfcfdfd4208188
Dzmitry <87767424+dzmitrySkr@users.noreply.github.com>
2026-01-08 13:05:59 +0300

    add css

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -6,13 +6,13 @@ import sm3 from '../../images/sm3.png'
 
 let SideMenu = () =>{
     return <div className="side_menu">
-        <div className={"sideMenu_title side_menu"}>Fraud Management</div>
+        <div className={"sideMenu_title"}>Fraud Management</div>
         <nav style={{display: 'flex', flexDirection: 'column', gap: 12}}>
             <NavLink
                 to="/"
                 end
                 className={({ isActive }) =>
-                    `side_menu${isActive ? ' active' : ''}`
+                    `side_menu_item${isActive ? ' active' : ''}`
                 }
             >
                 <img src={sm1} alt="Dashboard icon" />
@@ -21,7 +21,7 @@ let SideMenu = () =>{
 
             <NavLink to="/reports"
                      className={({isActive}) =>
-                         `side_menu${isActive ? ' active' : ''}`
+                         `side_menu_item${isActive ? ' active' : ''}`
                      }>
                 <img src={sm2} alt="Dashboard icon"/>
                 <span>Reports</span>
@@ -29,7 +29,7 @@ let SideMenu = () =>{
 
             <NavLink to="/transactions"
                      className={({isActive}) =>
-                         `side_menu${isActive ? ' active' : ''}`
+                         `side_menu_item${isActive ? ' active' : ''}`
                      }>
                 <img src={sm3} alt="Dashboard icon"/>
                 <span>TransActions</span>
